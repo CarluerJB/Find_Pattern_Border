bed01a9899cdcea32e49afc15b9f5ac8619bda00
CarluerJB <44376382+CarluerJB@users.noreply.github.com>
2023-05-30 16:21:46 +0200

    Update find_pattern_border.R
    Add a border check for pattern near beginning or end of sentence

--- find_pattern_border.R
+++ find_pattern_border.R
@@ -4,20 +4,26 @@ find_patern_border <- function(sentence, pattern, n=10){
   pos=data.frame(stri_locate_all(pattern=pattern, sentence, fixed=TRUE))
   # Create empty list to store results
   list_combination = c()
+  if(length(pos)==0){
+    return(list_combination)
+  }
   # Left loop
   for(i_left_border in c(1:n)){
     # Right loop
     for(j_right_border in c(1:n)){
-      new_combination = substr(sentence, pos$start-i_left_border, pos$end+j_right_border)
-      # Add new combination to the pool of combination
-      list_combination = c(list_combination, new_combination)
+      # Check for border located pattern
+      if(pos$start-i_left_border>1 & pos$end+j_right_border<=stri_length(sentence)){
+        new_combination = substr(sentence, pos$start-i_left_border, pos$end+j_right_border)
+        # Add new combination to the pool of combination
+        list_combination = c(list_combination, new_combination)
+      }
     }
   }
   return(list_combination)
 }
 
 # TEST
-sentence = "dkqsjfkljdjkdljkdkljJFKLJSKLDJAIAT4G36540OPJKDKDdazidjiozjdklqsjdjdioqjdzklcJIOZJIOJAIODJIOZJPKJZ"
+sentence = "dkqsjfkljdjkdljkdkljJFKLJSKLDJAIOPJKDKDdazidjiozjdklqsjdjdioqjdzklcJIOZJIOJAIODJIOZJPKAT4G36540JZ"
 pattern = "AT4G36540"
 list_combination = find_patern_border(sentence, pattern, n=5)
 
